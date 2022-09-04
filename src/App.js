74bdff1cd1ba6f5910fecc6ee4ea84da32e78ffe
Lesia-Sh <shvch.lv@gmail.com>
2022-09-04 14:03:06 -0400

    Edded logo(before) and bootstrap

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button className="btn btn-primary">Hello World</button>
       </header>
     </div>
   );
