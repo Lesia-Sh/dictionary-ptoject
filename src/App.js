2182b40bd7b67da9c0e4c46ce5630b11c9d89273
Lesia-Sh <shvch.lv@gmail.com>
2022-09-05 11:09:10 -0400

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import Dictionary from "./Dictionary";
 import logo from "./logo.jpg";
 import "./App.css";
 
@@ -5,9 +7,29 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button className="btn btn-primary">Hello World</button>
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer text-center">
+        This project was coded by{" "}
+        <a
+          href="https://meek-malabi-7397e3.netlify.app/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Lesia Shevchuk{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/Lesia-Sh/dictionary-ptoject"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
