c7bee60a68d88f8afb91378899e1eb515ada168b
Lesia-Sh <shvch.lv@gmail.com>
2022-09-09 15:40:28 -0400

    Styled the App

--- src/Example.js
+++ src/Example.js
@@ -4,7 +4,7 @@ export default function Example(props) {
   if (props.example) {
     return (
       <div className="Example">
-        <strong>Example:</strong>
+        <strong>Example: </strong>
         {props.example}
       </div>
     );
